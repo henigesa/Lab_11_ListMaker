a9f3377f0649a2d21c571998944f443c4c202dfc
henigesa <121961371+henigesa@users.noreply.github.com>
2023-05-01 12:32:37 -0400

    with all

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner in = new Scanner(System.in); //initialize the input memo
+    private static ArrayList<String> lines = new ArrayList<>(); //initialize the arraylist of memos
+    private static boolean savedSaver = false; //initialize that this variable has not been saved
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String menuPrompt = " A - Add \n D - Delete \n P - Print \n Q - Quit \n";
+        String cmd = "";
+
+        while (true) {
+            showList();
+            cmd = SafeInput.getRegExString(in, menuPrompt, "[AaDdPpQq]");
+            cmd = cmd.toUpperCase();
+
+            switch (cmd) {
+                case "A":
+                    add();
+                    savedSaver = true;
+                    break;
+                case "D":
+                    delete();
+                    savedSaver = true;
+                    break;
+                case "P":
+                    showList();
+                    break;
+                case "Q":
+                    if (!savedSaver || SafeInput.getYNConfirm(in, "Are you sure you would like to quit?")) {
+                        System.exit(0);
+                    }
+                    break;
+            }
+        }
+    }
+//shows the list in its current state
+    public static void showList() {
+        System.out.println("-----------------------------------------");
+        if (lines.size() == 0) {
+            System.out.println("\nThe list is empty right now\n");
+        } else {
+            for (int i = 0; i < lines.size(); i++) {
+                System.out.println("\t" + (i + 1) + ". " + lines.get(i));
+            }
+        }
+        System.out.println("-----------------------------------------");
+    }
+//adds a line to the list
+    public static void add() {
+        String lineItem = SafeInput.getNonZeroLengthString(in, "Enter the memo line");
+        lines.add(lineItem);
+    }
+//deletes a line from the list
+    private static void delete() {
+        System.out.println("-----------------------------");
+        if (lines.size() == 0) {
+            System.out.println("\nThere is nothing to delete right now\n");
+            System.out.println("--------------------------");
+            return;
+        }
+
+        int target = SafeInput.getRangeint(in, "Enter the number of the line you would like to delete", 1, lines.size());
+
+        lines.remove(target - 1);
     }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,89 @@
-package PACKAGE_NAME;public class SafeInput {
-}
+
+import java.util.Scanner;
+
+public class SafeInput
+{
+    public static int getRangeint(Scanner pipe, String prompt, int low, int high) {
+        boolean done = false;
+        int retValue = 0;
+        String trash = "";
+
+        do {
+            System.out.println("\n" + prompt + ": ");
+            if (pipe.hasNextInt()) {
+                retValue = pipe.nextInt();
+                pipe.nextLine();
+                if (retValue >= low && retValue <= high)
+                    done = true;
+                else
+                    System.out.println("You must enter a value in range: " + trash);
+
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("You must enter a value int not: " + trash);
+            }
+        } while (!done);
+
+        return retValue;
+    }
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        String value = "";
+        boolean retValue = false;
+        boolean done = false;
+
+        do {
+            System.out.print("\n" + prompt + "(y or n): ");
+            value = pipe.nextLine();
+
+            if (value.equalsIgnoreCase("y")) {
+                retValue = true;
+                done = true;
+            } else if (value.equalsIgnoreCase("n")) {
+                retValue = false;
+                done = true;
+            } else {
+                System.out.println("You must enter a Y or N.");
+            }
+        } while (!done);
+        return retValue;
+    }
+
+    public static String getRegExString(Scanner pipe, String prompt, String regEx) {
+        String retValue = "";
+        boolean done = false;
+        do {
+            System.out.print("\n" + prompt + " " + regEx + ": ");
+            retValue = pipe.nextLine();
+
+            if (retValue.matches(regEx)) {
+                done = true;
+            } else
+                System.out.println("You must enter an output that matches the pattern: " + regEx);
+
+        } while (!done);
+
+        return retValue;
+
+    }
+
+    public static String getNonZeroLengthString(Scanner pipe, String prompt) {
+        String retValue = "";
+        boolean done = false;
+
+        do {
+            System.out.print("\n" + prompt + ": ");
+            retValue = pipe.nextLine();
+
+            if (retValue.length() > 0)
+                done = true;
+            else
+                System.out.println("You must some characters!");
+
+
+        } while (!done);
+
+        return retValue;
+    }
+
+}//end class
